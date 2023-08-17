14c76599a62599144c38e5ff138ebace8af03b18
Natalia-Nowak <nowak.natalia.080@gmail.com>
2023-08-17 13:11:02 +0200

    completed tasks

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,44 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const images = [];
+
+for (const image of galleryItems) {
+  const liElement = document.createElement("li");
+
+  const divElement = document.createElement("div");
+  divElement.classList.add("gallery__item");
+
+  const aElement = document.createElement("a");
+  aElement.classList.add("gallery__link");
+
+  const imgElement = document.createElement("img");
+  imgElement.classList.add("gallery__image");
+  imgElement.src = image.preview;
+  imgElement.dataSource = image.original;
+  imgElement.alt = image.description;
+
+  liElement.append(divElement);
+  divElement.append(aElement);
+  aElement.append(imgElement);
+  images.push(liElement);
+}
+
+galleryList.append(...images);
+
+galleryList.addEventListener("click", (e) => {
+  console.log({ target: e.target, currentTarget: e.currentTarget });
+  const dataSource = e.target.dataSource;
+  if (typeof dataSource === "undefined") {
+    return;
+  }
+  console.log("obrazek");
+
+  const instance = basicLightbox.create(`
+    <img src="${dataSource}" width="800" height="600">
+`);
+
+  instance.show();
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryList = document.querySelector(".gallery");
+
+const images = [];
+
+for (const image of galleryItems) {
+  const liElement = document.createElement("li");
+
+  const aElement = document.createElement("a");
+  aElement.classList.add("gallery__item");
+  aElement.href = image.original;
+  aElement.dataset.title = "amdakd";
+  const imgElement = document.createElement("img");
+  imgElement.classList.add("gallery__image");
+  imgElement.src = image.preview;
+  imgElement.alt = image.description;
+
+  liElement.append(aElement);
+  aElement.append(imgElement);
+  images.push(liElement);
+}
+
+galleryList.append(...images);
+
+const lightbox = new SimpleLightbox("ul.gallery a", {});
+console.log(lightbox);
